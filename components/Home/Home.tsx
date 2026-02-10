84590af3a4d47805209ba73eb6c4dc3711eed7cc
Rahul-dev02 <rahulroa14320@gmail.com>
2026-02-11 02:58:47 +0530

    Services

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Services from './Services/Services'
 
 const Home = () => {
   return (
     <div className='overflow-hidden '>
      
      <Hero/>
+     <Services/>
     </div>
   )
 }
