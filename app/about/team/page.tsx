72bc45865170e3f80e65b811c2b4dacd2389f72e
Rahul-dev02 <rahulroa14320@gmail.com>
2026-02-11 02:27:02 +0530

    layout

---
+++ app/about/team/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div className='text-white text-5xl'>
+     Team page inside  about page .
+    </div>
+  )
+}
+
+export default TeamPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
-
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 const font=Inter({
       weight:["100","200",'300','400','500','600','700','800','900'],
       subsets:['latin'],
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body
         className={` ${font.className} antialiased bg-[#0d0d1f]`}
       >
+        <ResponsiveNav/>
         {children}
       </body>
     </html>
