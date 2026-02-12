29ba3cea8e6700309f293c8c41659ead46c9be3a
Rahul-dev02 <rahulroa14320@gmail.com>
2026-02-12 13:41:23 +0530

    Skills

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Services from './Services/Services'
+import Resume from './Resume/Resume'
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills'
 
 const Home = () => {
   return (
@@ -8,6 +11,9 @@ const Home = () => {
      
      <Hero/>
      <Services/>
+     <Resume />
+     <Projects/>
+     <Skills/>
     </div>
   )
 }
---
+++ components/Home/Skills/Skills.tsx
@@ -0,0 +1,66 @@
+"use client"
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import Tilt from 'react-parallax-tilt'
+const skills=[
+    {
+    name:'javascript',
+    icon:<SiJavascript/>,
+    percentage:89
+    },
+    {
+    name:'React.js',
+    icon:<SiReact/>,
+    percentage:92
+    },
+    {
+    name:'Next.js',
+    icon:<SiNextdotjs/>,
+    percentage:90
+   },
+    {
+    name:'Node.js',
+    icon:<SiNodedotjs/>,
+    percentage:88
+   },
+
+    {
+    name:'Tailwind css',
+    icon:<SiTailwindcss/>,
+    percentage:94
+   },
+    {
+    name:'TypeScript',
+    icon:<SiTypescript/>,
+    percentage:86
+   },
+    
+]
+const Skills = () => {
+  return (
+    <div className='text-white pt-16 pb-16'>
+      <h1 className='text-center text-2xl md:text-4xl xl:text-5xl  font-bold text-white'>
+       My <span className='text-cyan-300'>Skills</span>
+         </h1>
+         <div className=" w-[90%] sm:w-[70%] mx-auto  flex flex-wrap justify-between gap-1 mt-16">
+              {
+                skills.map((skil)=>{
+                     return(
+                      <Tilt key={skil.name} scale={1.5} transitionSpeed={400}>
+                        <div className="bg-[#14134145] text-center w-40 h-48 
+                          rounded-3xl flex flex-col
+                        items-center justify-center shadow-lg transition hover:scale-105">
+                            <div className="text-5xl  mb-4 text-gray-300">{skil.icon}</div>
+                            <div className="text-2xl font-semibold">{skil.percentage}%</div>
+                             <div className="text-purple-400 mt-1">{skil.name}</div>
+                        </div>
+                      </Tilt>
+                      
+                     )
+                })
+              }
+         </div>
+    </div>
+  )
+}
+
+export default Skills
